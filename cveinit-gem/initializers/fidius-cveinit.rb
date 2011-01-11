1f35620be32d03d1f6cef74b2c083aee3d93f35b
Dominik Menke <dmke@tzi.de>
2011-01-11 15:46:28 +0100

    huge cveinit improvement. should have been more than one commit...

---
+++ cveinit-gem/initializers/fidius-cveinit.rb
@@ -0,0 +1,6 @@
+databases = YAML.load_file File.join(RAILS_ROOT, 'config', 'database.yml')
+unless databases['cve_db']
+  raise ArgumentError.new "Missing CVE-DB configuration. Please add a section `cve_db' to your database.yml"
+end
+
+ActiveRecord::Base.establish_connection databases['cve_db']
